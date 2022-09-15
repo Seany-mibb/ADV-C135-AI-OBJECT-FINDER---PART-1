2ca6e6c9259e4cec4017de27e92097dbb9cacdd9
Seany-mibb <86206802+Seany-mibb@users.noreply.github.com>
2022-09-14 19:48:44 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
+var video = '';
+var Status = '';
+var object = '';
+
 function setup()
 {
     canvas = createCanvas(500, 400);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("status").innerHTML = 'Status: Object Detection has started';
+}
+
+function modelLoaded()
+{
+    console.log('Model is loaded')
+    Status = true;
+    object = document.getElementById("input").value;
+    console.log(object)
+}
+
+function draw()
+{
+    image(video, 0, 0, 500, 400);
 }
